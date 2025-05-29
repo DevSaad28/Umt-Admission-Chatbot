74b596d8cecd3c25d65470774ea596321d2e0062
HasnainAllahRakha <hasnainallahrakha58@gmail.com>
2025-05-29 14:38:30 +0500

    Initialize server package with essential dependencies and scripts

---
+++ Client/src/url.js
@@ -0,0 +1,6 @@
+const Base_Url = "http://localhost:5000/api";
+const User_Signup = `${Base_Url}/user/register`;
+const User_Login = `${Base_Url}/user/login`;
+const User_Search = `${Base_Url}/user`;
+
+export {Socket_Endpoint,User_Signup,User_Login,User_Search}
