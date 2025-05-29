01e874c2d0186863d150539a8093d66a42d78669
Saad <devsaad287@gmail.com>
2025-05-29 14:38:58 +0500

    Add framer-motion for animations and update Login component

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,12 @@
 // User Layout
 import Main from "../Layout/User/User.layout";
+import Login from "../Pages/User/Login.Page";
 
 // Router
 import { createBrowserRouter } from "react-router-dom";
 
 // User Pages
-import Login from "../Pages/User/Login.Page";
+
 import ChatBot from "../Components/User/ChatBot/ChatBot.component";
 
 const router = createBrowserRouter([
@@ -13,7 +14,8 @@ const router = createBrowserRouter([
     path: "/",
     element: <Main />,
     children: [{path : "", element : <Login/>},
-      {path : "/chatbot", element : <ChatBot/>}
+      {path : "/chatbot", element : <ChatBot/>},
+      
 
     ],
   },
