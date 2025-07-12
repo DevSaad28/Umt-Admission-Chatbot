de5932c1d53ac5d95862e56677e138a5ac45156a
Faizan Ahmad <faizana22333@gmail.com>
2025-07-12 20:16:29 +0500

    Implement code changes to enhance functionality and improve performance

--- Client/src/url.js
+++ Client/src/url.js
@@ -1,7 +1,7 @@
 const Base_Url = "http://localhost:5000";
 const API_BASE_URL = "http://localhost:8000";
-const User_Signup = `${Base_Url}/user/register`;
-const User_Login = `${Base_Url}/user/login`;
-const User_Search = `${Base_Url}/user`;
+const User_Signup = `${Base_Url}/api/user/register`;
+const User_Login = `${Base_Url}/api/user/login`;
+const User_Search = `${Base_Url}/api/user`;
 
 export { User_Signup, User_Login, User_Search, API_BASE_URL ,Base_Url};
